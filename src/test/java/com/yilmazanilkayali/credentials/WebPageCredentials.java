6a37a3345f6144cdfc54ec1dab9fb70b58b8815d
yilmazanilkayali <91636225+yilmazanilkayali@users.noreply.github.com>
2022-03-25 16:15:49 +0300

    Update WebPageCredentials.java
    update password

--- src/test/java/com/yilmazanilkayali/credentials/WebPageCredentials.java
+++ src/test/java/com/yilmazanilkayali/credentials/WebPageCredentials.java
@@ -2,6 +2,6 @@ package com.yilmazanilkayali.credentials;
 
 public class WebPageCredentials {
     public static final String USER_EMAIL = "ylmzanl@gmail.com";
-    public static final String PASSWORD = "16451645.y";
+    public static final String PASSWORD = "**********";
 
 }
